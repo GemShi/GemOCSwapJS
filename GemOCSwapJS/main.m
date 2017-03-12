569a5da5d5a5461de4b6d176a69033be32c34bed
GemShi <gemshi@GemShideMacBook-Pro.local>
2017-03-12 19:08:02 +0800

    Initial Commit

---
+++ GemOCSwapJS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GemOCSwapJS
+//
+//  Created by GemShi on 2017/3/12.
+//  Copyright © 2017年 GemShi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
